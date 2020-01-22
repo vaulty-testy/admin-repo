57f49f3f6758b71f96ad373996f265d8581f4a31
Chanuka <chanuka@techolution.com>
2020-01-22 18:12:01 +0800

    Humble initial commit

---
+++ repos.tf
@@ -0,0 +1,4 @@
+resource "github_repository" "admin-repo" {
+  name        = "admin-repo"
+  description = "Terrafrom based Github administration automation"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable github_token {
+  description = "Github Personal Token created under Settings > Developer Settings"
+  type        = string
+}
+
+variable github_organization {
+  description = "Name of the Github Organization you want to manage"
+  type        = string
+}
